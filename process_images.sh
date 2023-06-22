7a22aea9a63f8a9100bee5653daa42920502faa6
Piotr Patrzyk <piotr@patrzyk.me>
2023-06-22 19:47:17 +0200

    widths and heights

--- process_images.sh
+++ process_images.sh
@@ -20,7 +20,27 @@ exiftool -j \
     -ShutterSpeed \
     -ISO \
     -Flash \
+    -ImageWidth \
+    -ImageHeight \
     -Software \
     -Rights \
     -Copyright \
     $dir/* | jq '[.[] | {(.SourceFile):.}] | add' > exif.json
+
+cat exif.json | jq -r 'to_entries[] | "\(.key) \(.value.ImageWidth) \(.value.ImageHeight)"' | \
+while read line
+do
+    read -r path width height <<<$(echo $line)
+    echo " path $path width $width height $height"
+    if (( width > height ))
+    then
+        ratio=$(jq -n 1000/$width)
+        target_width=1000
+        target_height=$(jq -n "$height*$ratio | floor")
+    else
+        ratio=$(jq -n 1000/$height)
+        target_height=1000
+        target_width=$(jq -n "$width*$ratio | floor")
+    fi
+    echo " TARGET width $target_width height $target_height"
+done
